12803d992283105e05de037f9bb4cf6602a06411
Bober <towermanxxl@gmail.com>
2026-07-13 23:26:17 +0200

    add supabase client creation

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,27 @@
 import { Hono } from 'hono'
+import { createClient } from '@supabase/supabase-js'
+
+import { zValidator } from '@hono/zod-validator'
+import { z } from 'zod'
+
+const MemorySchema = z.object({
+  memories: z.string().min(1).array().min(1).max(10),
+  // for now just to test if it works
+  last_messages: z.string().array().min(1).max(10)
+})
 
 const app = new Hono()
 
-app.get('/add_memory', (c) => {
-  return c.text('Hello Hono!')
+const supabaseClient = createClient(
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_SECRET_KEY!
+)
+
+app.post('/add_memories', zValidator('json', MemorySchema), (ctx) => {
+  // 1. insert memories to database
+
+
+  return ctx.text('Hello Hono!')
 })
 
 export default app
