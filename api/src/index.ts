a718d7dabf9f597511369982b3cc654523aa92a4
Bober <towermanxxl@gmail.com>
2026-07-13 23:59:11 +0200

    update gitignore and mcp

--- api/src/index.ts
+++ api/src/index.ts
@@ -17,8 +17,20 @@ const supabaseClient = createClient(
   process.env.SUPABASE_SECRET_KEY!
 )
 
-app.post('/add_memories', zValidator('json', MemorySchema), (ctx) => {
+app.post('/add_memories', zValidator('json', MemorySchema), async (ctx) => {
+  const {memories, last_messages} = ctx.req.valid('json')
   // 1. insert memories to database
+  const {error} = await supabaseClient
+    .from('memories')
+    .insert(memories.map((item)=>({
+      content: item
+    })))
+
+  if (error) throw error
+
+  // 2. Run background worker used for entity extraction and graph making 
+  // TODO
+
 
 
   return ctx.text('Hello Hono!')
--- mcp/server.py
+++ mcp/server.py
@@ -1,13 +1,19 @@
+
 from mcp.server import MCPServer
+from pydantic import BaseModel, Field
 
 mcp = MCPServer("Demo")
 
+class MemorySchema(BaseModel):
+	memories: list[str] = Field(gt=1, description='Memories list created by ai from chat recent message')
+
+	last_messages: list[str] = Field(gt=0, lt=11, description='Last top 10 messages from chat. Used for graph creation')
 
 @mcp.tool()
-def test(param: str) -> str:
-    """Test tool."""
-    return f"Test tool worked! {param}"
+def add_memories(param: MemorySchema) -> str:
+	"""Test tool."""
+	return f"Test tool worked! {param}"
 
 
 if __name__ == "__main__":
-    mcp.run(transport='streamable-http', streamable_http_path='/streamable')
+	mcp.run(transport='streamable-http', streamable_http_path='/streamable')
