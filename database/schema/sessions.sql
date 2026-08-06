24c81979dcdd7d744cf6e684f7d4ed517f3154c1
Bober <towermanxxl@gmail.com>
2026-08-06 16:23:17 +0200

    add owner id to sessions

--- database/schema/sessions.sql
+++ database/schema/sessions.sql
@@ -2,6 +2,7 @@
 
 create table sessions (
   id uuid not null primary key default gen_random_uuid(),
+  owner_id uuid not null references auth.users(id) on delete cascade on update cascade,
   created_at timestamp with time zone not null default now()
 );
 GRANT ALL ON public.sessions TO service_role;
