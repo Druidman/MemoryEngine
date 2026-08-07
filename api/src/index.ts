f0f8e5e5880427d5c5e0901378ba1c6fa43c4680
Bober <towermanxxl@gmail.com>
2026-08-07 23:14:33 +0200

    vercel working

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,9 +2,9 @@ import { logger } from 'hono/logger'
 import { authMiddleware } from './middleware'
 import { cors } from 'hono/cors';
 import { app } from './app';
+import { getRequestListener } from '@hono/node-server';
 
 app.use(logger())
-
 console.log(process.env.APP_URL)
 app.use('*', cors({
   origin: process.env.APP_URL,
@@ -16,6 +16,6 @@ app.use('*', cors({
 app.use(authMiddleware)
 
 // register routes
-await import('./routes/router')
+import './routes/router'
 
-export default app;
+export default getRequestListener(app.fetch);
